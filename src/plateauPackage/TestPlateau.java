c7dae9072cdbbea36f57dd9141e7973a8d7beaa9
Victor Mougel <victor.mougel.etu@univ-lille.fr>
2021-06-01 21:04:55 +0200

    Test ajoutés

--- src/plateauPackage/TestPlateau.java
+++ src/plateauPackage/TestPlateau.java
@@ -4,66 +4,60 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.Ignore;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import joueurPackage.Joueur;
 
 public class TestPlateau{
 
     Plateau plateau;
+    Plateau pl1;
     Position p=new Position(1,1);
-    Position p2=new Position(1,2);
-    Position p3=new Position(3,2);
-    Position p4=new Position(3,8);
-    Position p5=new Position(3,7);
     Position p6=new Position(2,8);
+    Position p7=new Position(4,8);
+    static Joueur j1;
+    static Joueur j2;
+
+    @BeforeAll //Joueur a un id automatique donc on doit faire une beforeAll
+    public static void joueurs(){
+        j1=new Joueur(null,9);
+        j2=new Joueur(null,9);
+    }
 
     @BeforeEach
     public void initialisation(){
-        plateau=new Plateau(4);
+        plateau=new Plateau(3);
+        pl1 = new Plateau(3,3);
+        System.out.println("Joueur 1 "+j1.getJoueurId());
     }
 
-    @Test
-    public void testArcBloque(){
-
-        plateau.bloquer_arc(p,p2);  
-        assertTrue(plateau.arc_bloque(p,p2));
-        plateau.liberer_arc(p,p2);
-        assertFalse(plateau.arc_bloque(p, p2));
-        plateau.bloquer_arc(p3,p4);
-        assertFalse(plateau.arc_bloque(p5,p6));
-
-    }
 
     @Test
     public void testJoueur(){
-        plateau.placer_pion(p4,1);
-        assertFalse(plateau.case_libre(p4),"Position in array is free, it should not be");
+
+        plateau.placer_pion(j1.getPion(0),p);
+        assertFalse(plateau.case_libre(p),"Position in array is free, it should not be");
+        assertTrue(plateau.case_libre(p6),"Position in array is not free, it should be");
+        plateau.liberer_plateau(p);
         assertTrue(plateau.case_libre(p),"Position in array is not free, it should be");
-        plateau.liberer_plateau(p4);
-        assertTrue(plateau.case_libre(p4),"Position in array is not free, it should be");
     }
 
     @Test
     public void testgetArray(){
+
         assertArrayEquals(new int[]{23,2},plateau.getArray("23.2"));
         assertArrayEquals(new int[]{8,1},plateau.getArray("8.1.-1.3"));
         assertArrayEquals(new int[]{12,4},plateau.getArray("12.4.13.42.19"));
         assertArrayEquals(null, plateau.getArray("-1.4"));
         assertArrayEquals(null,plateau.getArray("Test.23"));
     }
-
-    @Test
-    public void testPassagePossible(){
-        assertTrue(p.passagePossible(p2));
-        assertTrue(p4.passagePossible(p5));
-        assertTrue(p5.passagePossible(p4));
-        assertFalse(p.passagePossible(p6));
-        assertTrue(p4.passagePossible(p6));
-        assertTrue(p6.passagePossible(p4));
-    }
     
     @Test
     public void testCreationArcs() {
+
     	Plateau pl1 = new Plateau(3,3);
     	assertTrue(pl1.getArcs(0, 1));
     	assertTrue(pl1.getArcs(7, 6));
@@ -80,30 +74,64 @@ public class TestPlateau{
     
     @Test
     public void testcouperArc() {
-    	Plateau pl2 = new Plateau(3,3);
-    	assertTrue(pl2.getArcs(2, 3));
-    	assertTrue(pl2.getArcs(3, 2));
-    	pl2.couperArc(new Position(1,4), new Position(1,3)); //faire +1 à chaque si ça marche pas ^^
-    	assertFalse(pl2.getArcs(2, 3));
-    	assertFalse(pl2.getArcs(3, 2));
+
+    	assertTrue(pl1.getArcs(2, 3));
+    	assertTrue(pl1.getArcs(3, 2));
+    	pl1.couperArc(new Position(1,4), new Position(1,3)); //faire +1 à chaque si ça marche pas ^^
+    	assertFalse(pl1.getArcs(2, 3));
+    	assertFalse(pl1.getArcs(3, 2));
     }
     
     @Test
     public void testArcsExiste() {
-    	Plateau pl3 = new Plateau(3,3);
-    	assertTrue(pl3.arcExiste(new Position(2,2), new Position(2,1)));
-    	assertTrue(pl3.arcExiste(new Position(2,6), new Position(1,6)));
-    	assertTrue(pl3.arcExiste(new Position(3,4), new Position(2,4)));
-    	assertFalse(pl3.arcExiste(new Position(2,6), new Position(1,5)));
-    	assertFalse(pl3.arcExiste(new Position(3,2), new Position(2,3)));
-    	assertFalse(pl3.arcExiste(new Position(1,6), new Position(2,5)));
+
+    	assertTrue(pl1.arcExiste(new Position(2,2), new Position(2,1)));
+    	assertTrue(pl1.arcExiste(new Position(2,6), new Position(1,6)));
+    	assertTrue(pl1.arcExiste(new Position(3,4), new Position(2,4)));
+    	assertFalse(pl1.arcExiste(new Position(2,6), new Position(1,5)));
+    	assertFalse(pl1.arcExiste(new Position(3,2), new Position(2,3)));
+    	assertFalse(pl1.arcExiste(new Position(1,6), new Position(2,5)));
     }
     
     @Test
     public void testCreerArc() {
-    	Plateau pl4 = new Plateau(3,3);
-    	assertFalse(pl4.getArcs(0, 4));
-    	pl4.creerArc(new Position(1,1), new Position(1,5));
-    	assertFalse(pl4.getArcs(0, 4));
+
+    	assertFalse(pl1.getArcs(0, 4));
+    	pl1.creerArc(new Position(1,1), new Position(1,5));
+    	assertTrue(pl1.getArcs(0, 4));
     }
+
+    @Test
+    public void testPositions(){
+        assertTrue(plateau.placementPossible(p));
+        assertTrue(plateau.placementPossible(p6));
+        assertFalse(plateau.placementPossible(p7));
+        plateau.placer_pion(j1.getPion(0),p6);
+        assertFalse(plateau.placementPossible(p6));
+
+    }
+
+   /* @Deprecated
+    @Test
+    public void testArcBloque(){
+
+        plateau.bloquer_arc(p,p2);  
+        assertTrue(plateau.arc_bloque(p,p2));
+        plateau.liberer_arc(p,p2);
+        assertFalse(plateau.arc_bloque(p, p2));
+        plateau.bloquer_arc(p3,p4);
+        assertFalse(plateau.arc_bloque(p5,p6));
+
+    }
+
+    @Deprecated
+    @Test
+    public void testPassagePossible(){
+        assertTrue(p.passagePossible(p2));
+        assertTrue(p4.passagePossible(p5));
+        assertTrue(p5.passagePossible(p4));
+        assertFalse(p.passagePossible(p6));
+        assertTrue(p4.passagePossible(p6));
+        assertTrue(p6.passagePossible(p4));
+    }*/
 }
