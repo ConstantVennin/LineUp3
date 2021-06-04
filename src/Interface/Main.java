e32593290270ba9f616c057a14f5e8e08d4a6b16
Bastien Lemarie <bastien.lemarie.etu@univ-lille.fr>
2021-06-04 10:19:16 +0200

    main interface

---
+++ src/Interface/Main.java
@@ -0,0 +1,34 @@
+package test_affichage_lineup;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	
+	public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        URL fxmlFileUrl = getClass().getResource("menu.fxml");
+        if (fxmlFileUrl == null) {
+                System.out.println("Impossible de charger le fichier fxml");
+                System.exit(-1);
+        }
+        loader.setLocation(fxmlFileUrl);
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Menu");
+        stage.show();
+	}
+	
+	
+
+	public static void main(String[] args) {
+                Application.launch(args);
+	}
+}
