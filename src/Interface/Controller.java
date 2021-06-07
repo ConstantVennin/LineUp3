187328c572c650e9912a60bb3633f607edd634b1
Bastien Lemarie <bastien.lemarie.etu@univ-lille.fr>
2021-06-07 12:41:12 +0200

    Replace Controller.java

--- src/Interface/Controller.java
+++ src/Interface/Controller.java
@@ -1,4 +1,4 @@
-package Interface;
+package test_affichage_lineup;
 
 import java.io.IOException;
 import java.net.URL;
@@ -20,13 +20,13 @@ import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 public class Controller {
+	public static String plateau="carre3";
 	@FXML
     Button button_solo,button_multi,button_triangle,button_carre,button_regle,button_video,button_quitter;
 	
 	public void actionButtonQuitter(ActionEvent event) {
-
 		Alert alert = new Alert(AlertType.CONFIRMATION);
-	      alert.setHeaderText("ï¿½tes vous sï¿½r de vouloir quitter le jeu ?");
+	      alert.setHeaderText("Êtes vous sûr de vouloir quitter le jeu ?");
 	 
 	      // option != null.
 	      Optional<ButtonType> option = alert.showAndWait();
@@ -41,13 +41,8 @@ public class Controller {
 	@FXML
 	public void actionButtonRegle(ActionEvent event) throws IOException {
 		Stage stage = (Stage) button_regle.getScene().getWindow();
-		stage.close();
 		FXMLLoader loader = new FXMLLoader();
         URL fxmlFileUrl = getClass().getResource("menu_regle.fxml");
-        if (fxmlFileUrl == null) {
-                System.out.println("Impossible de charger le fichier fxml");
-                System.exit(-1);
-        }
         loader.setLocation(fxmlFileUrl);
         Parent root = loader.load();
         Scene scene = new Scene(root);
@@ -57,20 +52,80 @@ public class Controller {
 		System.out.println("Regle choisis");
 	}
 	@FXML
-	public void actionButtonTriangle(ActionEvent event) {
-		System.out.println("Triangle choisis");
+	public void actionButtonTriangle3(ActionEvent event) {
+		plateau="triangle3";
+		System.out.println("Triangle 3 choisis");
 	}
 	@FXML
-	public void actionButtonCarre(ActionEvent event) {
-		System.out.println("Carrï¿½ choisis");
+	public void actionButtonCarre3(ActionEvent event) {
+		plateau="carre3";
+		System.out.println("Carré 3 choisis");	
 	}
 	@FXML
-	public void actionButtonSolo(ActionEvent event) {
-		System.out.println("Solo choisis");
+	public void actionButtonTriangle4(ActionEvent event) {
+		plateau="triangle4";
+		System.out.println("Triangle 4 choisis");
 	}
 	@FXML
-	public void actionButtonMulti(ActionEvent event) {
-		System.out.println("Multi choisis");
+	public void actionButtonCarre4(ActionEvent event) {
+		plateau="carre4";
+		System.out.println("Carré 4 choisis");	
+	}
+	@FXML
+	public void actionButtonSolo(ActionEvent event) throws IOException{
+		Stage stage = (Stage) button_regle.getScene().getWindow();
+		FXMLLoader loader = new FXMLLoader();
+        URL fxmlFileUrl = getClass().getResource(plateau+".fxml");
+        loader.setLocation(fxmlFileUrl);
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Line Up 3 Solo");
+        stage.show();
+		switch( plateau ) {
+			case "carre3":
+				 System.out.println("Carre 3 solo choisis");
+				 break;
+			case "carre4":
+				 System.out.println("Carre 4 solo choisis");
+				 break;
+			case "triangle3":
+				 System.out.println("Triangle 3 solo choisis");
+				 break;
+			case "triangle4":
+				 System.out.println("Triangle 4 solo choisis");
+				 break;
+			default:
+				 System.out.println("Carre 3 solo choisis");	
+		}
+	}
+	@FXML
+	public void actionButtonMulti(ActionEvent event) throws IOException {
+		Stage stage = (Stage) button_regle.getScene().getWindow();
+		FXMLLoader loader = new FXMLLoader();
+        URL fxmlFileUrl = getClass().getResource(plateau+".fxml");
+        loader.setLocation(fxmlFileUrl);
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Line Up 3 Multi");
+        stage.show();
+		switch( plateau ) {
+			case "carre3":
+				 System.out.println("Carre 3 multi choisis");
+				 break;
+			case "carre4":
+				 System.out.println("Carre 4 multi choisis");
+				 break;
+			case "triangle3":
+				 System.out.println("Triangle 3 multi choisis");
+				 break;
+			case "triangle4":
+				 System.out.println("Triangle 4 multi choisis");
+				 break;
+			default:
+				 System.out.println("Carre 3 multi choisis");	
+		}
 	}
 	@FXML
 	public void actionButtonVideo(ActionEvent event) {
@@ -86,6 +141,5 @@ public class Controller {
         video.show();
 
 	}
-
 	
 }
